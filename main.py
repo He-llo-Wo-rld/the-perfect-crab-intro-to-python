ea6a0363e755a33db42107db7c65706469466f32
He-llo-Wo-rld <serhiiforwork@gmail.com>
2025-08-11 16:10:56 +0100

    imroved_011

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from tasks.identity_011 import just_return_it_improved
 
 if __name__ == "__main__":
-    just_return_it_improved(10)
+    just_return_it_improved("str")
--- tasks/identity_011.py
+++ tasks/identity_011.py
@@ -1,5 +1,6 @@
 # Video alternative: https://vimeo.com/954334266/1ad4327868
 import logging
+from typing import Union
 
 import logconfig
 
@@ -14,9 +15,12 @@ def just_return_it(num):
 # Improved version
 
 
-def just_return_it_improved(num: int) -> int:
-    logger.info(f"just_return_it_improved({num}) called")
-    return num
+def just_return_it_improved(object: Union[int, float]) -> Union[int, float]:
+    if not isinstance(object, (int, float)):
+        logger.error(f"Invalid argument type: {type(object).__name__}")
+        return None
+    logger.info(f"just_return_it_improved({object}) called")
+    return object
 
 
 if __name__ == "__main__":
