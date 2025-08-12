8b6fac167d901e64baf3ddacf3c941823125e0a2
He-llo-Wo-rld <serhiiforwork@gmail.com>
2025-08-12 19:52:42 +0100

    completed 026

--- tasks/026_ifs.py
+++ tasks/026_ifs.py
@@ -1,5 +1,8 @@
 # Video alternative: https://vimeo.com/954334163/bf61706e77#t=0
+import os
+import sys
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from lib.helpers import check_that_these_are_equal
 
 # Programs often have to make decisions based on their input. For this, we use
@@ -10,7 +13,7 @@ from lib.helpers import check_that_these_are_equal
 leaves_on_the_tree = 0
 
 if leaves_on_the_tree == 0:
-  print("It must be winter — or a dead tree")
+    print("It must be winter — or a dead tree")
 
 # Let's break this down:
 
@@ -30,9 +33,9 @@ if leaves_on_the_tree == 0:
 # We can also have an `else` to cover the other case:
 
 if leaves_on_the_tree == 0:
-  print("It must be winter — or a dead tree")
+    print("It must be winter — or a dead tree")
 else:
-  print("This is a happy tree with nice leaves")
+    print("This is a happy tree with nice leaves")
 
 # When Python sees the `else:`, it will execute the next block of code only if
 # the condition evaluated to False.
@@ -46,50 +49,39 @@ else:
 print("")
 print("Function: is_first_of_the_month")
 
-def is_first_of_the_month(day_number):
-  # Return "First of the month!" if the day number is 1.
-  # Return "Not first of the month" otherwise.
-  pass
 
-check_that_these_are_equal(
-  is_first_of_the_month(1),
-  "First of the month!"
-)
+def is_first_of_the_month(day_number: int) -> str:
+    if not isinstance(day_number, int):
+        raise TypeError(f"Invalid type {type(day_number).__name__}, must be int")
+    if day_number == 1:
+        return "First of the month!"
+    return "Not first of the month"
 
-check_that_these_are_equal(
-  is_first_of_the_month(12),
-  "Not first of the month"
-)
+
+check_that_these_are_equal(is_first_of_the_month(1), "First of the month!")
+
+check_that_these_are_equal(is_first_of_the_month(12), "Not first of the month")
 
 # == Exercise Two ==
 
 print("")
 print("Function: has_five_chars")
 
-def has_five_chars(the_str):
-  # Return "STRING is five characters long" if the string is five characters
-  # long.
-  # Otherwise, return "Not five characters".
-  pass
 
-check_that_these_are_equal(
-  has_five_chars("ABCDE"),
-  "ABCDE is five characters long"
-)
+def has_five_chars(the_str: str) -> str:
+    if not isinstance(the_str, str):
+        raise TypeError(f"Invalid type {type(the_str).__name__}, must be str")
+    if len(the_str) == 5:
+        return f"{the_str} is five characters long"
+    return "Not five characters"
 
-check_that_these_are_equal(
-  has_five_chars("FORGE"),
-  "FORGE is five characters long"
-)
 
-check_that_these_are_equal(
-  has_five_chars("Nope"),
-  "Not five characters"
-)
+check_that_these_are_equal(has_five_chars("ABCDE"), "ABCDE is five characters long")
 
-check_that_these_are_equal(
-  has_five_chars("Nor this one"),
-  "Not five characters"
-)
+check_that_these_are_equal(has_five_chars("FORGE"), "FORGE is five characters long")
+
+check_that_these_are_equal(has_five_chars("Nope"), "Not five characters")
+
+check_that_these_are_equal(has_five_chars("Nor this one"), "Not five characters")
 
 # When you're done, move on to 027_comparison.py
